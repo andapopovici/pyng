9b52fe8e4b0c3fd40ea24f70ae3b16805a01a0c1
andapopovici <andap2002@gmail.com>
2017-07-10 22:03:59 +0100

    The ball is now a rectangle

--- game.py
+++ game.py
@@ -14,14 +14,14 @@ size = (MAX_WIDTH, MAX_HEIGHT)
 PADDLE_WIDTH = 10
 PADDLE_HEIGHT = 100
 # the X coord of the right paddle
-RIGHT_PADDLE_X = 695
+RIGHT_PADDLE_X = 690
 PADDLE_MIN_Y = 0
-PADDLE_MAX_Y = 500
+PADDLE_MAX_Y = 400
 # the speed with which we move the paddle
 PADDLE_SPEED = 5
 
 # Ball constants
-BALL_DIMENSION = 6
+BALL_DIMENSION = 5
 
 pygame.init()
 screen = pygame.display.set_mode(size)
@@ -46,29 +46,27 @@ print("initial ball position ", ball_x, ball_y)
 ball_change_x = 5
 ball_change_y = 5
 
-def drawPaddle(topX, topY, bottomX, bottomY):
-    # TODO check how this works with rectangle instead
-    pygame.draw.line(screen, WHITE, [topX, topY], [bottomX, bottomY], PADDLE_WIDTH)
+def drawPaddle(topX, topY):
+    pygame.draw.rect(screen, WHITE, [topX, topY, PADDLE_WIDTH, PADDLE_HEIGHT])
 
-def drawRightPaddle(topY, bottomY):
-    drawPaddle(RIGHT_PADDLE_X, topY, RIGHT_PADDLE_X, bottomY)
+def drawRightPaddle(topY):
+    drawPaddle(RIGHT_PADDLE_X, topY)
 
-def movePaddleBySpeed(topY_coord, bottomY_coord, speed):
+def movePaddleBySpeed(topY_coord, speed):
     topY_coord += speed
+
     if topY_coord < PADDLE_MIN_Y:
         topY_coord = PADDLE_MIN_Y
-        bottomY_coord = PADDLE_HEIGHT
 
-    bottomY_coord += speed
-    if bottomY_coord > PADDLE_MAX_Y:
-        topY_coord = PADDLE_MAX_Y - PADDLE_HEIGHT
-        bottomY_coord = PADDLE_MAX_Y
+    if topY_coord > PADDLE_MAX_Y:
+        topY_coord = PADDLE_MAX_Y
 
-    return topY_coord, bottomY_coord
+    return topY_coord
 
 while not done:
     # --- Limit to 60 frames per second
     clock.tick(60)
+
     # --- Main event loop
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
@@ -85,8 +83,8 @@ while not done:
     # above this, or they will be erased with this command.
     screen.fill(BLACK)
 
-    topY_coord, bottomY_coord = movePaddleBySpeed(topY_coord, bottomY_coord, speed)
-    drawRightPaddle(topY_coord, bottomY_coord)
+    topY_coord = movePaddleBySpeed(topY_coord, speed)
+    drawRightPaddle(topY_coord)
 
     pygame.draw.circle(screen, WHITE, (ball_x, ball_y), BALL_DIMENSION, 0)
 
@@ -95,10 +93,8 @@ while not done:
 
     # Bounce the rectangle if needed
     if ball_y > MAX_HEIGHT - BALL_DIMENSION or ball_y < 0:
-        print('almost off edge vertically', ball_y, 'need to go', 'up' if ball_y > MAX_HEIGHT - BALL_DIMENSION else 'down')
         ball_change_y = ball_change_y * -1
-    if ball_x > MAX_WIDTH - BALL_DIMENSION or ball_x < 0:
-        print('almost off edge horizontally', ball_x, 'need to go', 'left' if ball_x > MAX_WIDTH - BALL_DIMENSION else 'right')
+    if ball_x > MAX_WIDTH - PADDLE_WIDTH or ball_x < 0:
         ball_change_x = ball_change_x * -1
 
     # --- Go ahead and update the screen with what we've drawn.
